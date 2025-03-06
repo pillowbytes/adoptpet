3ffe3b7ae48b8ed9a78d654e36586640ef247bb7
Sidney Junior <siid.live@gmail.com>
2025-03-06 17:53:47 -0300

    testing

---
+++ db/migrate/20250306201201_create_pets.rb
@@ -0,0 +1,14 @@
+class CreatePets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.integer :age
+      t.string :species
+      t.string :gender
+      t.string :size
+      t.boolean :is_available
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# API KEYs
+
+DOG API
+# APIDoc: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+# KEY: live_k0ypRZufgumt7H7WLpR8rgq2UCjAJb4HzNgcVYhWeH5Ffy4J89AlFZ392bIDN9xX
+DOG API
+# APIDoc: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=FJkYOq9tW
+# KEY: live_YxdQyRuxOBL1GwIg7j7rEEkV2hqTxvPCBQKUqqqHOelb84AHXcV7D533glXetRxP
