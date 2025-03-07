bd3aa844614f92f5c31c6b20e57bd21ebf9c8443
Guilherme Barbosa <lg.barbosa12@gmail.com>
2025-03-07 16:02:39 -0300

    Foi feito a criação do def show pet show e a viwer

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,12 @@
 class PetsController < ApplicationController
+
+  def show
+  @pet = Pet.find(params[:id])
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :species, :gender, :size, :image_url)
+  end
 end
