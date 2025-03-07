914a91c5242fdd1bae07c18a9915435a3520d50e
pedro cozzi <pedrocozzi2@gmail.com>
2025-03-07 15:44:20 -0300

    pets new/create controller e params feito

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,18 @@
 class PetsController < ApplicationController
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.save
+    redirect_to pet_path(@pet)
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :species, :gender, :size, :image_url)
+  end
 end
