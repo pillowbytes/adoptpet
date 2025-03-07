11b95c94bf7aaf1f28a89c66747a29b7f42ef927
Sidney Junior <siid.live@gmail.com>
2025-03-07 15:24:51 -0300

    Created Pets controller, added edit and update methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,22 +3,4 @@ class PagesController < ApplicationController
 
   def home
   end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,17 @@
 class PetsController < ApplicationController
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :species, :gender, :size, :image_url)
+  end
 end
