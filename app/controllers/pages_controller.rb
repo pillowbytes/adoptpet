8ac1718801a59844aff9b04aee59db1d763ecd5b
Sidney Junior <siid.live@gmail.com>
2025-03-07 14:44:31 -0300

    Created pet controller with basic empty routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,22 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
