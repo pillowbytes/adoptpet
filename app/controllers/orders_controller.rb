0a2fc8cab0933c832cc3daf1445efaa69e20b2a2
gabriela pereira <gabriela-barcelos@uol.com.br>
2025-03-10 12:01:58 -0300

    created show and create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,17 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      redirect_to order_path(@order), notice: "Cadastro de adoção criado!"
+    else
+      render :new
+    end
   end
 
   def destroy
