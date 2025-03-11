b78b8218a2c93121f18963f727ae3416f55062cf
Sidney Junior <siid.live@gmail.com>
2025-03-11 17:04:00 -0300

    fixed cloudnary image index

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -75,4 +74,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.active_storage.service = :cloudinary
 end
